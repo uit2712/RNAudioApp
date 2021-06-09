1f87a5cb3278c1b28d4f116433a23aba40bfe8a1
uit2712 <phamquangvi456@gmail.com>
2021-06-09 13:23:53 +0700

    update Root component

--- navigators/components/root/home/index.tsx
+++ navigators/components/root/home/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 
 import { BottomTabBarOptions, BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomTabDescriptorMap, BottomTabNavigationEventMap } from '@react-navigation/bottom-tabs/lib/typescript/src/types';
 import { DrawerHomeContext, SoundPlayerContext, } from '@context-api/index';
+import { NavigationHelpers, ParamListBase, RouteProp, TabNavigationState } from '@react-navigation/native';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -18,9 +20,6 @@ import TabSongsNavigators from './tab-songs';
 import TabSoundPlayerDetailNavigator from './tab-detail';
 import { navigate } from '@navigators/config/root';
 import { useDrawHomeSettings } from '@hooks/index';
-import { useGetAllAlbums } from '@hooks/albums-screen-hooks';
-import { useGetAllArtists } from '@hooks/artists-screen-hooks';
-import { useGetAllMusicFiles } from '@hooks/songs-screen-hooks';
 
 const DrawerHome = createBottomTabNavigator<DrawerHomeParams>();
 
@@ -94,9 +93,6 @@ const iconSize = 25;
 
 function DrawerHomeNavigator() {
     const settings = useDrawHomeSettings();
-    useGetAllMusicFiles();
-    useGetAllArtists();
-    useGetAllAlbums();
 
     return (
         <DrawerHomeContext.Provider value={settings}>
@@ -144,59 +140,89 @@ function ListTabs({ state, descriptors, navigation }: BottomTabBarProps<BottomTa
         {
             ({ isShowTabBar }) => isShowTabBar === true && (
                 <View style={{ flexDirection: 'row' }}>
-                    {state.routes.map((route, index) => {
-                        const screen = screens[index];
-                        const { options } = descriptors[route.key];
-                        const isFocused = state.index === index;
-
-                        const onPress = () => {
-                            const event = navigation.emit({
-                                type: 'tabPress',
-                                target: route.key,
-                                canPreventDefault: true,
-                            });
-
-                            if (!isFocused && !event.defaultPrevented) {
-                                navigation.navigate(route.name);
-                            }
-                        };
-
-                        const onLongPress = () => {
-                            navigation.emit({
-                                type: 'tabLongPress',
-                                target: route.key,
-                            });
-                        };
-
-                        if (screen.isVisible === false) {
-                            return <TouchableOpacity key={index}></TouchableOpacity>
-                        }
-
-                        return (
-                            <TouchableOpacity
-                                key={index}
-                                accessibilityRole="button"
-                                accessibilityState={isFocused ? { selected: true } : {}}
-                                accessibilityLabel={options.tabBarAccessibilityLabel}
-                                testID={options.tabBarTestID}
-                                onPress={onPress}
-                                onLongPress={onLongPress}
-                                style={{
-                                    flex: 1,
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                }}
-                            >
-                                {screen.icon({ color: screen.getColor(isFocused) })}
-                                {screen.label({ title: screen.title, color: screen.getColor(isFocused) })}
-                            </TouchableOpacity>
-                        );
-                    })}
+                    {
+                        state.routes.map((route, index) => {
+                            const props = {
+                                key: index,
+                                index,
+                                route,
+                                navigation,
+                                screen: screens[index],
+                                descriptors,
+                                state,
+                            };
+                            return (
+                                <ListTabItem {...props} />
+                            )
+                        })
+                    }
                 </View>
             )
         }
         </DrawerHomeContext.Consumer>
     )
+}
+
+function ListTabItem({
+    navigation,
+    route,
+    screen,
+    index,
+    descriptors,
+    state,
+}: {
+    navigation: NavigationHelpers<ParamListBase, BottomTabNavigationEventMap>,
+    route: RouteProp<ParamListBase, string>,
+    screen: IDrawerHomeNavigatorScreen,
+    index: number,
+    descriptors: BottomTabDescriptorMap,
+    state: TabNavigationState<ParamListBase>,
+}) {
+    const { options } = descriptors[route.key];
+    const isFocused = state.index === index;
+
+    function onPress() {
+        const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+        });
+
+        if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+        }
+    };
+
+    function onLongPress() {
+        navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+        });
+    };
+
+    if (screen.isVisible === false) {
+        return <TouchableOpacity key={index}></TouchableOpacity>
+    }
+
+    return (
+        <TouchableOpacity
+            key={index}
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            {screen.icon({ color: screen.getColor(isFocused) })}
+            {screen.label({ title: screen.title, color: screen.getColor(isFocused) })}
+        </TouchableOpacity>
+    );
 }
 
 function MiniPlayer() {
@@ -231,53 +257,82 @@ function MiniPlayer() {
                 }}
                 onPress={goToSoundPlayerDetail}
             >
-                <TouchableOpacity style={{ width: '60%', }} onPress={goToSoundPlayerDetail}>
-                    <Text style={{ color: 'white', fontWeight: 'bold' }}>{player.currentAudioInfo.name}</Text>
-                    <Text style={{ color: 'white' }}>{player.currentAudioInfo.other}</Text>
-                </TouchableOpacity>
-                <View style={{ flexDirection: 'row' }}>
-                    <TouchableOpacity onPress={player.previous}>
-                        <AntDesign
-                            name='stepbackward'
-                            size={30}
-                            color='white'
-                        />
-                    </TouchableOpacity>
-                    {
-                        player.status === 'play' ? (
-                            <TouchableOpacity onPress={player.pause}>
-                                <AntDesign
-                                    name='pausecircle'
-                                    size={30}
-                                    color='white'
-                                />
-                            </TouchableOpacity>
-                        ) : (
-                            <TouchableOpacity onPress={player.play}>
-                                <AntDesign
-                                    name='play'
-                                    size={30}
-                                    color='white'
-                                />
-                            </TouchableOpacity>
-                        )
-                    }
-                    <TouchableOpacity onPress={player.next}>
-                        <AntDesign
-                            name='stepforward'
-                            size={30}
-                            color='white'
-                        />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={{ marginLeft: 10, }}>
-                        <AntDesign
-                            name='heart'
-                            size={30}
-                            color='white'
-                        />
-                    </TouchableOpacity>
-                </View>
+                <MiniPlayerSummaryInfo goToSoundPlayerDetail={goToSoundPlayerDetail}/>
+                <MiniPlayerActions/>
             </TouchableOpacity>
+            <MiniPlayerDuration/>
+        </>
+    )
+}
+
+function MiniPlayerSummaryInfo({
+    goToSoundPlayerDetail
+}: {
+    goToSoundPlayerDetail: () => void
+}) {
+    const player = React.useContext(SoundPlayerContext);
+    
+    return (
+        <TouchableOpacity style={{ width: '60%', }} onPress={goToSoundPlayerDetail}>
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>{player.currentAudioInfo.name}</Text>
+            <Text style={{ color: 'white' }}>{player.currentAudioInfo.other}</Text>
+        </TouchableOpacity>
+    )
+}
+
+function MiniPlayerActions() {
+    const player = React.useContext(SoundPlayerContext);
+
+    return (
+        <View style={{ flexDirection: 'row' }}>
+            <TouchableOpacity onPress={player.previous}>
+                <AntDesign
+                    name='stepbackward'
+                    size={30}
+                    color='white'
+                />
+            </TouchableOpacity>
+            {
+                player.status === 'play' ? (
+                    <TouchableOpacity onPress={player.pause}>
+                        <AntDesign
+                            name='pausecircle'
+                            size={30}
+                            color='white'
+                        />
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity onPress={player.play}>
+                        <AntDesign
+                            name='play'
+                            size={30}
+                            color='white'
+                        />
+                    </TouchableOpacity>
+                )
+            }
+            <TouchableOpacity onPress={player.next}>
+                <AntDesign
+                    name='stepforward'
+                    size={30}
+                    color='white'
+                />
+            </TouchableOpacity>
+            <TouchableOpacity style={{ marginLeft: 10, }}>
+                <AntDesign
+                    name='heart'
+                    size={30}
+                    color='white'
+                />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+function MiniPlayerDuration() {
+    const player = React.useContext(SoundPlayerContext);
+    return (
+        <>
             {
                 player.duration > 0 && (
                     <LinearProgress
