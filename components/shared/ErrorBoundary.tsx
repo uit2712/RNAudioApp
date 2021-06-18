02ec62604eec0e38ea855ebb925e14427e9c6bae
uit2712 <phamquangvi456@gmail.com>
2021-06-18 20:25:13 +0700

    add ErrorBoundary

---
+++ components/shared/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import React, { ErrorInfo, ReactNode } from 'react';
+
+interface Props {
+    children: ReactNode;
+    message: string;
+}
+
+interface State {
+    hasError: boolean;
+    defaultMessage?: string;
+}
+class ErrorBoundary extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            hasError: false,
+            defaultMessage: 'Something went wrong.',
+        };
+    }
+
+    static getDerivedStateFromError(_: Error): State {
+        // Update state so the next render will show the fallback UI.
+        return {
+            hasError: true,
+        };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+        // You can also log the error to an error reporting service
+        // logErrorToMyService(error, errorInfo);
+    }
+
+    render() {
+        const error = this.props.message ?? this.state.defaultMessage ?? 'Error';
+        if (this.state.hasError) {
+            // You can render any custom fallback UI
+            return <h1>{error}</h1>;
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
