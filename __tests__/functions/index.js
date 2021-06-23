691e45d7b750a3d5020e0916649c8c4b7a2a3791
uit2712 <phamquangvi456@gmail.com>
2021-06-23 07:33:16 +0700

    add test: listToMatrix

--- __tests__/functions/index.js
+++ __tests__/functions/index.js
@@ -1,5 +1,25 @@
-import { formatTimeString } from '@functions/index';
+import { formatTimeString, listToMatrix } from '@functions/index';
 
 test('formatTimeString: format time 35000 miliseconds is 00:02:30', () => {
     expect(formatTimeString(35000)).toBe('00:00:35');
+});
+
+test('formatTimeString: format time 350000 miliseconds is 00:05:50', () => {
+    expect(formatTimeString(350000)).toBe('00:05:50');
+});
+
+test('listToMatrix: input ([1, 2, 3], 2) get [[1, 2], [3]]', () => {
+    expect(listToMatrix([1, 2, 3], 2)).toStrictEqual([[1, 2], [3]]);
+});
+
+test('listToMatrix: input ([]) get []', () => {
+    expect(listToMatrix([], 2)).toStrictEqual([]);
+});
+
+test('listToMatrix: input ([], -1) get []', () => {
+    expect(listToMatrix([], -1)).toStrictEqual([]);
+});
+
+test('listToMatrix: input ([], 0) get []', () => {
+    expect(listToMatrix([], 0)).toStrictEqual([]);
 });
